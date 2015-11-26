173f5b1062a938f4933d382b5e1aa3d2a9c896db
David Martin <david@martinmacias.com>
2015-11-26 17:57:24 +0100

    Bumped version and changed app token

--- library.demo/src/main/java/net/pubnative/library/demo/MainActivity.java
+++ library.demo/src/main/java/net/pubnative/library/demo/MainActivity.java
@@ -24,7 +24,8 @@ public class MainActivity extends ActionBarActivity implements PubnativeActivity
 
     // OLD_API: 62cb87940b3a70cf67a200c3b443b47c20fd00b0a1aece21ec435d8e0521eb60
     // NEW_API: 6651a94cad554c30c47427cbaf0b613a967abcca317df325f363ef154a027092
-    private final String APP_TOKEN = "6651a94cad554c30c47427cbaf0b613a967abcca317df325f363ef154a027092";
+    // SPOTX_VIDEO: 78fcce3f082f263348db271b0f15ecb83d259491b383f31bc0a8ba84897311b8
+    private final String APP_TOKEN = "78fcce3f082f263348db271b0f15ecb83d259491b383f31bc0a8ba84897311b8";
 
     ImageView nativeIcon   = null;
     ImageView nativeBanner = null;
