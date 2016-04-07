c79db2afe84750a2d808280e8789fe0c7c9f1011
Anshuman Jaiswal <anshumansworld@gmail.com>
2016-04-07 17:25:06 +0530

    renamed key to data in DataModel

--- library/src/main/java/net/pubnative/library/request/model/PubnativeAPIV3DataModel.java
+++ library/src/main/java/net/pubnative/library/request/model/PubnativeAPIV3DataModel.java
@@ -5,5 +5,21 @@ import java.util.HashMap;
 public class PubnativeAPIV3DataModel {
 
     protected String                    type;
-    protected HashMap<String, String>   dictionary;
+    protected HashMap<String, String>   data;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public HashMap<String, String> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, String> data) {
+        this.data = data;
+    }
 }
