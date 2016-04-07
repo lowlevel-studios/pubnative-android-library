3b542bb583144309ff85fdd33ff2eeef192abf03
Anshuman Jaiswal <anshumansworld@gmail.com>
2016-04-07 17:23:51 +0530

    added models for AssetType, MetaType, Banner & Icon

---
+++ library/src/main/java/net/pubnative/library/request/model/PubnativeAssetType.java
@@ -0,0 +1,12 @@
+package net.pubnative.library.request.model;
+
+public interface PubnativeAssetType {
+
+    String TITLE        = "title";
+    String DESCRIPTION  = "description";
+    String CTA          = "cta";
+    String RATING       = "rating";
+    String ICON         = "icon";
+    String BANNER       = "banner";
+
+}
---
+++ library/src/main/java/net/pubnative/library/request/model/PubnativeMetaType.java
@@ -0,0 +1,9 @@
+package net.pubnative.library.request.model;
+
+public interface PubnativeMetaType {
+
+    String POINTS       = "points";
+    String REVENU_MODEL = "revenuemodel";
+    String CAMPAIGN_ID  = "campaignid";
+    String CREATIVE_ID  = "creativeid";
+}
