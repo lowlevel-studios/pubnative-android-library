7696e6e868d40c15ae9af30d3f21178d1a42c797
Anshuman Jaiswal <anshumansworld@gmail.com>
2016-04-08 18:29:26 +0530

    added getters in PubnativeImage

--- library/src/main/java/net/pubnative/library/request/model/PubnativeImage.java
+++ library/src/main/java/net/pubnative/library/request/model/PubnativeImage.java
@@ -6,4 +6,15 @@ public class PubnativeImage {
     protected int height;
     protected String url;
 
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
