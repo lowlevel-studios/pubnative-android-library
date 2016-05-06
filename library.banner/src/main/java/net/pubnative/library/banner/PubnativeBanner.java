e23043cea2703025d3753de29228666160cab9c0
Artsiom Aniskou <artsiom.aniskou@pubnative.net>
2016-05-06 15:34:38 +0200

    Changes: Fix small view issues. Add listeners.

--- library.banner/src/main/java/net/pubnative/library/banner/PubnativeBanner.java
+++ library.banner/src/main/java/net/pubnative/library/banner/PubnativeBanner.java
@@ -4,13 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import net.pubnative.library.request.PubnativeAsset;
 import net.pubnative.library.request.PubnativeRequest;
 import net.pubnative.library.request.model.PubnativeAdModel;
 
@@ -24,6 +31,15 @@ public class PubnativeBanner implements PubnativeRequest.Listener,
     protected String                mAppToken;
     protected Size                  mBannerSize;
     protected Position              mBannerPosition;
+    protected Boolean               mIsLoading = false;
+    protected Boolean               mIsShown = false;
+    protected Listener              mListener;
+    protected PubnativeAdModel      mAdModel;
+    // Banner view
+    protected ViewGroup             mContainer;
+    protected TextView              mTitle;
+    protected TextView              mDescription;
+    protected ImageView             mIcon;
 
     public enum Size {
         BANNER_50,
@@ -35,15 +51,65 @@ public class PubnativeBanner implements PubnativeRequest.Listener,
         BOTTOM
     }
 
+    /**
+     * Interface for callbacks related to the banner view behaviour
+     */
+    public interface Listener {
+
+        /**
+         * Called whenever the banner finished loading an ad
+         *
+         * @param banner banner that finished the load
+         */
+        void onPubnativeBannerLoadFinish(PubnativeBanner banner);
+
+        /**
+         * Called whenever the banner failed loading an ad
+         *
+         * @param banner banner that failed the load
+         * @param exception    exception with the description of the load error
+         */
+        void onPubnativeBannerLoadFail(PubnativeBanner banner, Exception exception);
+
+        /**
+         * Called when the Banner was just shown on the screen
+         *
+         * @param banner banner that was shown in the screen
+         */
+        void onPubnativeBannerShow(PubnativeBanner banner);
+
+        /**
+         * Called when the banner impression was confrimed
+         *
+         * @param banner banner which impression was confirmed
+         */
+        void onPubnativeBannerImpressionConfirmed(PubnativeBanner banner);
+
+        /**
+         * Called whenever the banner was clicked by the user
+         *
+         * @param banner banner that was clicked
+         */
+        void onPubnativeBannerClick(PubnativeBanner banner);
+
+        /**
+         * Called whenever the banner was removed from the screen
+         *
+         * @param banner banner that was hidden
+         */
+        void onPubnativeBannerHide(PubnativeBanner banner);
+    }
+
     public PubnativeBanner(Context context, String appToken, Size bannerSize, Position bannerPosition) {
-        RelativeLayout banner = null;
 
         mContext = context;
         mAppToken = appToken;
         mBannerSize = bannerSize;
         mBannerPosition = bannerPosition;
+
+        RelativeLayout banner;
         LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final ViewGroup viewGroup = (ViewGroup) ((ViewGroup) ((Activity) mContext).findViewById(android.R.id.content)).getChildAt(0);
+        mContainer = (ViewGroup) ((ViewGroup) ((Activity) mContext).findViewById(android.R.id.content)).getChildAt(0);
         switch (bannerSize) {
             case BANNER_90:
                 banner = (RelativeLayout) layoutInflater.inflate(R.layout.pubnative_banner_tablet, null);
@@ -53,6 +119,7 @@ public class PubnativeBanner implements PubnativeRequest.Listener,
                 banner = (RelativeLayout) layoutInflater.inflate(R.layout.pubnative_banner_phone, null);
                 break;
         }
+
         RelativeLayout bannerView = (RelativeLayout) banner.findViewById(R.id.pubnative_banner_view);
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)bannerView.getLayoutParams();
         switch (bannerPosition) {
@@ -64,41 +131,168 @@ public class PubnativeBanner implements PubnativeRequest.Listener,
                 params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
                 break;
         }
+        mTitle = (TextView) banner.findViewById(R.id.pubnative_banner_title);
+        mDescription = (TextView) banner.findViewById(R.id.pubnative_banner_description);
+        mIcon = (ImageView) banner.findViewById(R.id.pubnative_banner_image);
         bannerView.setLayoutParams(params);
-
-        viewGroup.addView(banner);
+        mContainer.addView(banner);
     }
 
+    /**
+     * Sets a callback listener for this interstitial object
+     *
+     * @param listener valid PubnativeInterstitial.Listener object
+     */
+    public void setListener(Listener listener) {
+
+        Log.v(TAG, "setListener");
+        mListener = listener;
+    }
+
+    /**
+     * Starts loading an ad for this interstitial
+     */
     public void load() {
-        //TODO: load method implementation
+        Log.v(TAG, "load");
+        if (TextUtils.isEmpty(mAppToken)) {
+            invokeLoadFail(new Exception("PubnativeBanner - load error: app token is null or empty"));
+        } else if (mContext == null) {
+            invokeLoadFail(new Exception("PubnativeBanner - load error: context is null or empty"));
+        } else if (mIsLoading) {
+            Log.w(TAG, "load - The ad is loaded or being loaded, dropping this call");
+        } else if (isReady()) {
+            invokeLoadFinish();
+        } else {
+            mIsShown = false;
+            mIsLoading = true;
+            PubnativeRequest request = new PubnativeRequest();
+            request.setParameter(PubnativeRequest.Parameters.APP_TOKEN, mAppToken);
+            String[] assets = new String[] {
+                    PubnativeAsset.TITLE,
+                    PubnativeAsset.DESCRIPTION,
+                    PubnativeAsset.ICON,
+                    PubnativeAsset.BANNER,
+                    PubnativeAsset.CALL_TO_ACTION,
+                    PubnativeAsset.RATING
+            };
+            request.setParameterArray(PubnativeRequest.Parameters.ASSET_FIELDS, assets);
+            request.start(mContext, this);
+        }
     }
 
+    /**
+     * Method that checks if the banner is ready to be shown in the screen
+     *
+     * @return true if the banner can be shown false if not
+     */
+    public boolean isReady() {
+
+        Log.v(TAG, "setListener");
+        return mAdModel != null;
+    }
+
+    /**
+     * Shows, if not shown and ready the cached interstitial over the current activity
+     */
     public void show() {
-        //TODO: show method implementation
-        render();
+        Log.v(TAG, "show");
+        if (mIsShown) {
+            Log.w(TAG, "show - the ad is already shown, ");
+        } else if (isReady()) {
+            render();
+        } else {
+            Log.e(TAG, "show - Error: the banner is not yet loaded");
+        }
     }
 
+    /**
+     * Destroy the current interstitial resetting all the cached data and removing any possible interstitial in the screen
+     */
     public void destroy() {
-        //TODO: destroy method implementation
+        Log.v(TAG, "destroy");
+        hide();
+        mAdModel = null;
+        mIsShown = false;
+        mIsLoading = false;
     }
+
+    //==============================================================================================
+    // Helpers
+    //==============================================================================================
 
     protected void hide() {
-        //TODO: hide method implementation
+        Log.v(TAG, "hide");
+        if (mIsShown) {
+            RelativeLayout rootBannerView = (RelativeLayout) mContainer.findViewById(R.id.pubnative_banner_root_view);
+            mContainer.removeView(rootBannerView);
+            mAdModel.stopTracking();
+            mIsShown = false;
+            invokeHide();
+        }
     }
 
     protected void render() {
-        //TODO: render method implementation
-
-        //invokeShow();
+        Log.v(TAG, "render");
+        mTitle.setText(mAdModel.getTitle());
+        mDescription.setText(mAdModel.getDescription());
+        Picasso.with(mContext).load(mAdModel.getIconUrl()).into(mIcon);
+        mAdModel.startTracking(mContainer, mContainer.findViewById(R.id.pubnative_banner_button), this);
+        invokeShow();
     }
 
-    protected int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = mContext.getResources().getDimensionPixelSize(resourceId);
+    //==============================================================================================
+    // Callback helpers
+    //==============================================================================================
+    protected void invokeLoadFinish() {
+
+        Log.v(TAG, "invokeLoadFinish");
+        mIsLoading = false;
+        if (mListener != null) {
+            mListener.onPubnativeBannerLoadFinish(this);
         }
-        return result;
+    }
+
+    protected void invokeLoadFail(Exception exception) {
+
+        Log.v(TAG, "invokeLoadFail", exception);
+        mIsLoading = false;
+        if (mListener != null) {
+            mListener.onPubnativeBannerLoadFail(this, exception);
+        }
+    }
+
+    protected void invokeShow() {
+
+        mIsShown = true;
+        Log.v(TAG, "invokeShow");
+        if (mListener != null) {
+            mListener.onPubnativeBannerShow(this);
+        }
+    }
+
+    protected void invokeImpressionConfirmed() {
+
+        Log.v(TAG, "invokeImpressionConfirmed");
+        if (mListener != null) {
+            mListener.onPubnativeBannerImpressionConfirmed(this);
+        }
+    }
+
+    protected void invokeClick() {
+
+        Log.v(TAG, "invokeClick");
+        if (mListener != null) {
+            mListener.onPubnativeBannerClick(this);
+        }
+    }
+
+    protected void invokeHide() {
+
+        Log.v(TAG, "invokeHide");
+        mIsShown = false;
+        if (mListener != null) {
+            mListener.onPubnativeBannerHide(this);
+        }
     }
 
     //==============================================================================================
@@ -109,12 +303,19 @@ public class PubnativeBanner implements PubnativeRequest.Listener,
 
     @Override
     public void onPubnativeRequestSuccess(PubnativeRequest request, List<PubnativeAdModel> ads) {
-
+        Log.v(TAG, "onPubnativeRequestSuccess");
+        if (ads == null || ads.size() == 0) {
+            invokeLoadFail(new Exception("PubnativeBanner - load error: no-fill"));
+        } else {
+            mAdModel = ads.get(0);
+            invokeLoadFinish();
+        }
     }
 
     @Override
     public void onPubnativeRequestFailed(PubnativeRequest request, Exception ex) {
-
+        Log.v(TAG, "onPubnativeRequestFailed");
+        invokeLoadFail(ex);
     }
 
     //----------------------------------------------------------------------------------------------
@@ -123,16 +324,18 @@ public class PubnativeBanner implements PubnativeRequest.Listener,
 
     @Override
     public void onPubnativeAdModelImpression(PubnativeAdModel pubnativeAdModel, View view) {
-
+        Log.v(TAG, "onPubnativeAdModelImpression");
+        invokeImpressionConfirmed();
     }
 
     @Override
     public void onPubnativeAdModelClick(PubnativeAdModel pubnativeAdModel, View view) {
-
+        Log.v(TAG, "onPubnativeAdModelClick");
+        invokeClick();
     }
 
     @Override
     public void onPubnativeAdModelOpenOffer(PubnativeAdModel pubnativeAdModel) {
-
+        Log.v(TAG, "onPubnativeAdModelOpenOffer");
     }
 }
--- library.demo/src/main/java/net/pubnative/library/demo/BannerActivity.java
+++ library.demo/src/main/java/net/pubnative/library/demo/BannerActivity.java
@@ -9,7 +9,10 @@ import android.widget.RelativeLayout;
 import net.pubnative.library.banner.PubnativeBanner;
 import net.pubnative.library.demo.utils.Settings;
 
-public class BannerActivity extends Activity {
+public class BannerActivity extends Activity implements PubnativeBanner.Listener {
+
+    public static final String TAG = BannerActivity.class.getSimpleName();
+    private RelativeLayout mLoaderContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +20,7 @@ public class BannerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banner);
 
-        findViewById(R.id.root).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                Log.d("Test Application Banner", "Banner Activity got click action");
-            }
-        });
+        mLoaderContainer = (RelativeLayout) findViewById(R.id.activity_native_container_loader);
 
     }
 
@@ -33,10 +29,48 @@ public class BannerActivity extends Activity {
                 this,
                 Settings.getAppToken(),
                 PubnativeBanner.Size.BANNER_50,
-                PubnativeBanner.Position.TOP
+                PubnativeBanner.Position.BOTTOM
         );
-        //banner.setListener(this);
+        banner.setListener(this);
         banner.load();
         banner.show();
+    }
+
+    //==============================================================================================
+    // Callbacks
+    //==============================================================================================
+    // PubnativeBanner.Listener
+    //----------------------------------------------------------------------------------------------
+    @Override
+    public void onPubnativeBannerLoadFinish(PubnativeBanner banner) {
+        Log.v(TAG, "onPubnativeBannerLoadFinish");
+        banner.show();
+    }
+
+    @Override
+    public void onPubnativeBannerLoadFail(PubnativeBanner banner, Exception exception) {
+        Log.v(TAG, "onPubnativeBannerLoadFail", exception);
+        mLoaderContainer.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onPubnativeBannerShow(PubnativeBanner banner) {
+        Log.v(TAG, "onPubnativeBannerShow");
+        mLoaderContainer.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onPubnativeBannerImpressionConfirmed(PubnativeBanner banner) {
+        Log.v(TAG, "onPubnativeBannerImpressionConfirmed");
+    }
+
+    @Override
+    public void onPubnativeBannerClick(PubnativeBanner banner) {
+        Log.v(TAG, "onPubnativeBannerClick");
+    }
+
+    @Override
+    public void onPubnativeBannerHide(PubnativeBanner banner) {
+        Log.v(TAG, "onPubnativeBannerHide");
     }
 }
